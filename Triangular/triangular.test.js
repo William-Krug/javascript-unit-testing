d1d4d550f5e374b57e31e48751d0b009a6aacc15
William Krug <william.p.krug@gmail.com>
2021-05-03 13:55:48 -0500

    added bad input and missing input tests

--- Triangular/triangular.test.js
+++ Triangular/triangular.test.js
@@ -18,7 +18,26 @@ describe('the triangular function', () => {
   /*
     Bad Input Path
   */
+  test('should return 3 if the passed input is the string "2"', () => {
+    const result = triangular('2');
+    expect(result).toBe(3);
+  });
+
+  test('should return 21 if the passed input is the string "6"', () => {
+    const result = triangular('6');
+    expect(result).toBe(21);
+  });
+
+  test('should fail if given a letter character or string', () => {
+    expect(() => {
+      triangular('hi');
+    }).toThrow(`Invalid argument passed. Please enter a number.`);
+  });
+
   /*
     Missing Input Path
   */
+  test('should fail if given no arguments', () => {
+    triangular();
+  }).toThrow(`No argument passed. Please enter a number.`);
 });
