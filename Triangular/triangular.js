67fbed00b3b1271f3caf421aad0ce341fd0971fc
William Krug <william.p.krug@gmail.com>
2021-05-03 14:06:27 -0500

    passing non-number character inputs test

--- Triangular/triangular.js
+++ Triangular/triangular.js
@@ -4,16 +4,24 @@
  *
  *    T(n) = (n(n+1))/2
  *
- * @param {number} number input value to calculate triangular number
+ * @param {number} inputNumber input value to calculate triangular number
  * @returns {number} calculated triangular number
  */
-function triangular(number) {
+function triangular(inputNumber) {
   let triangularNumber;
 
   // Check for missing argument
-  if (number === undefined) {
+  if (inputNumber === undefined) {
     throw new Error(`No argument passed. Please enter a number.`);
   }
+
+  // Check for non number character inputs (eg. 'hi')
+  const convertedInputNumber = Number(inputNumber);
+
+  if (isNaN(convertedInputNumber)) {
+    throw new Error(`Invalid argument passed. Please enter a number.`);
+  }
+
   return triangularNumber;
 }
 
